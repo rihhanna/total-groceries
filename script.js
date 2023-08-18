02e41a1cf1eef7d1a278c1ed5b2986dc913f39c3
rihhana <hrihhana@gmail.com>
2023-08-18 20:41:05 +0300

    answrs of total-groceries

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Good Luck 💪🏾
+function calculateTotalPrice(groceries) {
+    return groceries.reduce((total, item) => total + item.price * item.quantity, 0);
+  }
+  const groceries = [
+    { product: 'Milk', quantity: 1, price: 1.23 },
+    { product: 'Eggs', quantity: 12, price: 0.98 },
+    { product: 'Bread', quantity: 2, price: 2.45 },
+  ];
+  
+  console.log(calculateTotalPrice(groceries)); // Output: 10.29
+    
